16a30fa72048dc6fe4d742c040200c6aa784bff8
akg <akg.bit@gmail.com>
2020-04-25 17:52:30 +0530

    Google Analytics added

--- scripts/essentials.js
+++ scripts/essentials.js
@@ -1,49 +1,12 @@
 $(document).ready(function () {
 
-    var jqxhr = $.get("https://api.covid19india.org/v2/state_district_wise.json", function (data) {
-
-        console.log("fetching data from https://api.covid19india.org/v2/state_district_wise.json");
-
-        var totalConfirmedCases = 0;
-        var totalActiveCases = 0;
-        var totalDeceasedCases = 0;
-        var totalRecovedCases = 0;
-
-
-        // State Loop
-        $.each(data, function (key, value) {
-            var stateData = value;
-            var stateDistrictData = stateData.districtData;
-
-            // State District Loop
-            $.each(stateDistrictData, function (key, value) {
-                totalConfirmedCases += value.confirmed;
-                totalActiveCases += value.active;
-                totalDeceasedCases += value.deceased;
-                totalRecovedCases += value.recovered;
-            });
-
-        });
-
-        $("#totalConfirmed").html(totalConfirmedCases);
-        $("#totalActive").html(totalActiveCases);
-        $("#totalDeceased").html(totalDeceasedCases);
-        $("#totalRecovered").html(totalRecovedCases);
-
-
-        console.log("data successfully received");
-
-    })
-        .done(function () {
-            //alert("second success");
-        })
-        .fail(function () {
-            alert("error");
-        })
-        .always(function () {
-            //alert("finished");
-        });
-
+    $(document).ajaxStart(function () {
+        //$("#loading").show();
+        console.log("show");
+    }).ajaxStop(function () {
+        //$("#loading").hide();
+        
+    });
 
     // fetch data from api
     var jqxhr = $.get("https://api.covid19india.org/resources/resources.json", function (data) {
@@ -59,6 +22,7 @@ $(document).ready(function () {
 
     })
         .done(function () {
+            console.log("hide");
             //alert("second success");
         })
         .fail(function () {
