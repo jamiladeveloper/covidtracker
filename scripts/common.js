44866f6184fe285ff53bea8d3b919f534effefdd
akg <akg.bit@gmail.com>
2020-04-25 13:13:39 +0530

    Email script added

--- scripts/common.js
+++ scripts/common.js
@@ -8,4 +8,24 @@ $(document).ready(function(){
     var day = now.getDay();
 
     $("#date").html(dayText[day - 1] + ", " + date + "/" + month + "/" + year);
+
+
+    
+
+    
 });
+
+function sendemail() {
+    console.log("sending email");
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "jamiladeveloper@gmail.com",
+        Password : "4FBDAD978D00BF7CEF2075BD36BF5543FAE3",
+        To : "adddyyy89@gmail.com",
+        From : "jamiladeveloper@gmail.com",
+        Subject : "Test Email from Elastic Email",
+        Body : "This is a test email"
+    }).then(
+      message => alert(message)
+    );
+}
