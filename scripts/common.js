11ba33e7fe8b96128fdc7acf7c4655adf28ea2df
akg <akg.bit@gmail.com>
2020-04-25 21:09:06 +0530

    Flying action button css for mobile

--- scripts/common.js
+++ scripts/common.js
@@ -41,18 +41,5 @@ $(document).ready(function(){
     
 });
 
-function sendemail() {
-    console.log("sending email");
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "jamiladeveloper@gmail.com",
-        Password : "4FBDAD978D00BF7CEF2075BD36BF5543FAE3",
-        To : "adddyyy89@gmail.com",
-        From : "jamiladeveloper@gmail.com",
-        Subject : "Test Email from Elastic Email",
-        Body : "This is a test email"
-    }).then(
-      message => alert(message)
-    );
-}
+
 
